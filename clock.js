d3964fe156d8519f6f69eacdfa672f4ec7dd1b66
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-07-09 01:45:20 -0300

    Extracting Clock

--- clock.js
+++ clock.js
@@ -1,13 +1,21 @@
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world!</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function tick() {
     const element = (
-        <div>
-            <h1>Hello world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-        </div>
+        <Clock date={new Date()} />
     );
     root.render(element);
 }
 
-setInterval(tick, 0);
+setInterval(tick, 1000);
