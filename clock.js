88527261aa3e82b634b4c10d58f41a2db7cfe97c
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-07-09 01:48:06 -0300

    Added State

--- clock.js
+++ clock.js
@@ -1,15 +1,24 @@
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello world!</h1>
-                <h2>It is {this.props.date.toLocaleTimeString()}</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
             </div>
         );
     }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Clock />,
+    document.getElementById("root")
+)
 
 function tick() {
     const element = (
